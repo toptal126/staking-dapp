878d4884d96fc53f43d6eb0200b41985620c8a05
OzoneClub <cryptokuinka@gmail.com>
2022-03-11 09:35:44 +0000

    staking work

---
+++ utils/constant.js
@@ -0,0 +1,8 @@
+export const StakingTokenAddress = "0x8ef90905B79a111cF5322a1A8F95900E7568E73A";
+export const RewardTokenAddress = "0x912944d85E92Cc096db1f2A37F42bf47b248d828";
+export const StakingRewardsAddress =
+    "0x464c438FA2AafEB15F4773E14297979b4F2516F1";
+
+export const MAX_UINT256 =
+    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+export const MAX_INT = 2 ** 256 - 1;
